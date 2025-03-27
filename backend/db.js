862b8ec3d457eb569c05055a93e9a0aed2ca46fb
Abhijitam01 <abhijitamdubey31@gmail.com>
2025-03-27 10:09:58 +0000

    user schema

---
+++ backend/db.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('')
+
+const userSchema = new mongoose.Schema({
+    username :{
+        type : String,
+        required : true,
+        trim: true,
+        minLength : 6,
+        maxLength : 30,
+        unique : true,
+        lowercase : true
+    } ,
+    password : {
+        type : String ,
+        required : true,
+        minLenght : 6,
+        maxLength : 30
+    } ,
+    firstName : {
+        type : String ,
+        required : true,
+        minLenght : 3,
+        maxLength : 30
+    },
+    lastName : {
+        type : String ,
+        required : true,
+        minLenght : 3,
+        maxLength : 30
+    }
+
+})
+
+const UserModel = new mongoose.model("User" , userSchema)
+
+module.exports = {
+    UserModel
+}
